73c991e82694dd7a5d5d4bf6975147afddbc95e0
Claudio <claudiozurita87@gmail.com>
2017-08-30 13:05:57 -0300

    Se modifico nuevamente App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,6 @@ import logo from './logo.svg';
 import Saludo from './components/Saludo'
 import './App.css';
 
-/*class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;*/
-
 class App extends React.Component{
   render(){
     // Podria crear variables con const y agregar estado o lo que sea :).
