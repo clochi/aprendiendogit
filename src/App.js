f2b827924ec808a4b789da754f8b59b6d04bd09a
Claudio <claudiozurita87@gmail.com>
2017-08-30 13:04:31 -0300

    Se modifico App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Saludo from './components/Saludo'
 import './App.css';
 
-class App extends Component {
+/*class App extends Component {
   render() {
     return (
       <div className="App">
@@ -15,6 +16,19 @@ class App extends Component {
         </p>
       </div>
     );
+  }
+}
+
+export default App;*/
+
+class App extends React.Component{
+  render(){
+    // Podria crear variables con const y agregar estado o lo que sea :).
+    return(
+      <div className="contenedor-github">
+        <Saludo name="Claudio"></Saludo>
+      </div>
+    )
   }
 }
 
